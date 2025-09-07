5baf60ff32d8fc3b3c5c3dfdb842028322c318b1
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-09-07 09:45:12 +0545

    add routes for Login, ResetPassword, and Emailverify components in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import  Home from './pages/homepage'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import Emailverify from './pages/Emailverify'
 
 function App() {
   return (
     <div>
        <Routes>
         <Route path='/' element= {<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/reset-password' element= {<ResetPassword/>}/>
+        <Route path='/email-verify' element= {<Emailverify/>}/>
        </Routes>
       </div>
   )
