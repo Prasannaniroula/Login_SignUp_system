5d85d5d1f34aab013e0c189431f0f984e9303c27
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-09-08 09:20:09 +0545

    add Navbar and Header components; include SVG assets for icons and logo; update Homepage layout

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 function homepage() {
   return (
-    <div>homepage</div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+      <Navbar/>
+      <Header/>
+    </div>
   )
 }
 
